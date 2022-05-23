77c732c0a5fee9cd5dfa631c7d942d52b777aac0
InfoBlock <36295870+InfoBlock@users.noreply.github.com>
2022-05-23 14:17:05 +0200

    Update TMPAPI.js

--- API/js/TMPAPI.js
+++ API/js/TMPAPI.js
@@ -3,10 +3,11 @@ async function getUser() {
         const response = await fetch('https://api.truckersmp.com/v2/player', {
             method: 'GET',
             headers: {
-                Accept: 'text/html',
-                Cookie: 'Version=1',
                 mode: 'no-cors',
                 credentials: 'include',
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST,GET,PUT,DELETE",
+                "Access-Control-Allow-Headers": "Authorization, Lang, Origin, X-Requested-With, Content-Type, Accept"
             },
         });
 
@@ -22,27 +23,3 @@ async function getUser() {
 }
 
 getUser();
-
-/*const xhr = new XMLHttpRequest()
-
-xhr.open("GET", "https://api.truckersmp.com/v2/player/")
-xhr.setRequestHeader("Content-Type", "application/json")
-xhr.setRequestHeader("Accept", "text/html")
-xhr.setRequestHeader("Cookie", "Version=1")
-xhr.setRequestHeader("mode", "cors")
-xhr.setRequestHeader("credentials", "include")
-xhr.send()
-
-xhr.onload = function() {
-    if (xhr.status === 200) {
-        data = JSON.parse(xhr.responseText)
-        console.log(data.name)
-        console.log(data.steamID)
-    } else if (xhr.status === 404) {
-        console.log("No records found")
-    }
-}
-
-xhr.onerror = function() {
-    console.log("Network error occured")
-}*/
